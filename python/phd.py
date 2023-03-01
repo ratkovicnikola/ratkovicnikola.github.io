31dec2dbe5b50ba533e2c94140fc240847710ace
nratkovicsymphony <nikola.ratkovic@symphony.is>
2023-03-01 12:32:56 +0100

    Added method for partial model calculation

--- python/phd.py
+++ python/phd.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import math
 
 # xInput = [0.06,0.08,0.1,0.12,0.14,0.16,0.18,0.2,0.24,0.28,0.32,0.36,0.4,0.44,0.48,0.52,0.56,0.6,0.64,0.68,0.72,0.76,0.8,0.82,0.84,0.86,0.88,0.9,0.92]
 # yInput = [0.9452,0.7306,0.6237,0.4570,0.3858,0.2874,0.2044,0.1393,0.0187,-0.1101,-0.2288,-0.2921,-0.3609,-0.3840,-0.3970,-0.3901,-0.3613,-0.3328,-0.2796,-0.2031,-0.0835,0.1281,0.3096,0.3788,0.4540,0.5625,0.6848,0.8332,0.9311]
@@ -56,6 +57,28 @@ def showPlot(title, xInput, yInput, yResult):
   plt.grid(True)
   plt.show()
 
+def calculateDualModel(xInput, yInput):
+  power = 2
+  polynomialCoefficients = np.polyfit(xInput, yInput, power)
+  yResult = calculateY(xInput, power, polynomialCoefficients)
+  correlationCoefficient = pd.Series(yInput).corr(pd.Series(yResult))
+  title = getPolynomialText(polynomialCoefficients, power) + '\n' + getCorrelationText(power, correlationCoefficient)
+  showPlot(title, xInput, yInput, yResult)
+
+def calculatePartialModel(xInput, yInput):
+  xResult = []
+  resultLength = 6 # this will be used for dynamic calculation for best correlation coefficient
+  for i in range(len(xInput)):
+    if (i < resultLength):
+      xResult.append(math.log10(xInput[i]))
+  power = 1
+  yInputShort = yInput[0:resultLength]
+  polynomialCoefficients = np.polyfit(xResult, yInputShort, power)
+  yResult = calculateY(xResult, power, polynomialCoefficients)
+  correlationCoefficient = pd.Series(yInputShort).corr(pd.Series(yResult))
+  title = getPolynomialText(polynomialCoefficients, power) + '\n' + getCorrelationText(power, correlationCoefficient)
+  showPlot(title, xResult, yInputShort, yResult)
+
 if (len(sys.argv) != 3):
   print('Please input x and y values as command arguments')
 else:
@@ -67,13 +90,8 @@ else:
     yInput = list(map(float, args2))
 
     if (len(xInput) == len(yInput)):
-      power = 2
-      polynomialCoefficients = np.polyfit(xInput, yInput, power)
-      yResult = calculateY(xInput, power, polynomialCoefficients)
-      correlationCoefficient = pd.Series(yInput).corr(pd.Series(yResult))
-
-      title = getPolynomialText(polynomialCoefficients, power) + '\n' + getCorrelationText(power, correlationCoefficient)
-      showPlot(title, xInput, yInput, yResult)
+      # calculateDualModel(xInput, yInput)
+      calculatePartialModel(xInput, yInput)
     else:
       print('X and Y values are not the same length')
   except Exception as err:
